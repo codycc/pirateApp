d0a65bf7e8915b5b9c4293468e72dac99c976421
Cody Condon <codycondon50@gmail.com>
2018-11-03 13:03:23 -0400

    set up view for storeVC

---
+++ piratesApp/Controller/StoreVC.swift
@@ -0,0 +1,21 @@
+//
+//  StoreVC.swift
+//  piratesApp
+//
+//  Created by Cody Condon on 2018-11-03.
+//  Copyright © 2018 Cody Condon. All rights reserved.
+//
+
+import UIKit
+
+class StoreVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+
+}
